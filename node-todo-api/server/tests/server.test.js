5376084cd8e3be1f2c893139d991d5f3ca49835b
R T <knightofbombay@yahoo.com>
2018-07-24 20:24:44 -0400

    GET test case

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {TodoModel} = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
 beforeEach ((done) => {
-    TodoModel.remove({}).then(() => done());
+    TodoModel.remove({}).then(() => {
+        TodoModel.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +32,7 @@ describe('POST /todos', () => {
                 return done(err);
             }
 
-            TodoModel.find().then((todos) => {
+            TodoModel.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -43,9 +51,21 @@ describe('POST /todos', () => {
             }
 
             TodoModel.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e) => done(e));
         });
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
     });
 });
