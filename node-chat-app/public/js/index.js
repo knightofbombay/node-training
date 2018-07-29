ed5daa58c298a622f3241bfea44a7c6b47734b95
R T <knightofbombay@yahoo.com>
2018-07-29 19:01:15 -0400

    Scroll to bottom if user is close to bottom

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+    // Selectors
+    let messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('connected to server');
 });
@@ -18,6 +35,7 @@ socket.on('newMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -30,16 +48,8 @@ socket.on('newLocationMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
-    // let li = jQuery('<li></li>');
-    // let a = jQuery('<a target="_blank">My current location</a>');
-    
-    // li.text(`${message.from}: ${formattedTime}`);
-    
-    // a.attr('href', message.url);
-    // li.append (a);
-
-    // jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
